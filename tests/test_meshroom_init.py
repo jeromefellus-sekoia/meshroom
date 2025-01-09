b84f6b55c57c1214b247c3cade712c1eaa918aa5
Jerome Fellus <jerome.fellus@sekoia.fr>
2025-01-09 13:32:42 +0100

    s

--- tests/test_meshroom_init.py
+++ tests/test_meshroom_init.py
@@ -1,8 +1,10 @@
+from shutil import rmtree
 from meshroom.model import init_project
 from tests.conftest import PROJECT_DIR
 
 
 def test_meshroom_init():
+    rmtree(PROJECT_DIR, ignore_errors=True)
     init_project(PROJECT_DIR)
     assert (PROJECT_DIR / ".gitignore").is_file()
     assert (PROJECT_DIR / "README.md").is_file()
